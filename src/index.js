806b2d14bf7bb25bafd0222a9a369a84aad1870b
Geoffrey Litt <gklitt@gmail.com>
2020-05-02 13:58:48 -0400

    menu bar app and terrible icon

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 const { app, BrowserWindow, clipboard, Notification } = require('electron');
+const { menubar } = require('menubar');
+
 const fetch = require('node-fetch');
 const path = require('path');
+
+const mb = menubar({
+  'dir': './src',
+  'icon': './src/icons/clipboard.png'
+});
+
+mb.on('ready', () => {
+  console.log('app is ready');
+});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -32,14 +24,6 @@ app.on('window-all-closed', () => {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
 
